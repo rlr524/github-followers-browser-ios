b3a5ebcded90151da34a4b32d9bfcf2b434cc332
Rob Ranf <rob@emiyaconsulting.com>
2024-01-25 19:15:09 -0800

    adding button and text field reusables

---
+++ GithubFollowersBrowser/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GithubFollowersBrowser
+//
+//  Created by Rob Ranf on 1/25/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // A required initializer that handles inititializtion via storyboard,
+    // even though we're not using storyboard here.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GithubFollowersBrowser/GFTextField.swift
@@ -0,0 +1,44 @@
+//
+//  GFTextField.swift
+//  GithubFollowersBrowser
+//
+//  Created by Rob Ranf on 1/25/24.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(placeholderText: String) {
+        super.init(frame: .zero)
+        self.placeholder = placeholderText
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        // A standard UILable color that will be black on light mode and white on dark mode.
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+    }
+}
